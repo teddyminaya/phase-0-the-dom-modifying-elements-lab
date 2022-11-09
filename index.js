ea0b3e39831d7c002b226ccf7cd5b1af9807bdeb
Teddy Minaya <teddyminaya@gmail.com>
2022-11-09 13:33:19 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const main=document.getElementById("main")
+main.remove();
+const newHeader= document.createElement("h1");
+newHeader.setAttribute("id","victory");
+document.textContent= ("Teddy is the champion");
